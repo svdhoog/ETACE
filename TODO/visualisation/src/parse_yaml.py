deb51e7cb18a82573f97f4977eb36bfe16a13a5e
Krishna Devkota <krdevkota1@gmail.com>
2017-01-11 00:23:06 +0100

    all fixed parse_yaml.py

--- TODO/visualisation/src/parse_yaml.py
+++ TODO/visualisation/src/parse_yaml.py
@@ -8,36 +8,24 @@ with open("config.yaml", 'r') as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-#print d
+
+def process_parsed_values(d): 
+    indices = ['set','run','major','minor']    
+    for i in indices:
+        if 'range' in str(d[i][0]):
+            x = d[i][1]
+            if len(x)<3: x.append(1) 
+            d[i] = range(x[0],x[1],x[2])
+    return d
 
 
 for key in d.keys():
-    d_plt = d[key]    
-    #for d in d_plt.itervalues():    
-        #print d
-    if 'range' in str(d_plt['set'][0]):
-        x = d_plt['set'][1]
-        if len(x)<3: x.append(1) 
-        d_plt['set'] = range(x[0],x[1],x[2])
-        print d_plt
-    if 'range' in str(d_plt['run'][0]):
-        x = d_plt['run'][1]
-        if len(x)<3: x.append(1)       
-        d_plt['run'] = range(x[0],x[1],x[2])
-        print d_plt
-    if 'range' in str(d_plt['major'][0]):
-        x = d_plt['major'][1]
-        if len(x)<3: x.append(1)      
-        d_plt['major'] = range(x[0],x[1],x[2])
-        print d_plt 
-    if 'range' in str(d_plt['minor'][0]):
-        x = d_plt['minor'][1]
-        if len(x)<3: x.append(1)        
-        d_plt['minor'] = range(x[0],x[1],x[2])
-        print d_plt  
-    
+    d_plt = d[key] 
+    param = process_parsed_values(d_plt)
+
+#print param
 
 
 
-    #print d_plt['major'][1]
-    
+
+
