ef700abe755411d1c21c75b6829fc622a8184039
Krishna Devkota <krdevkota1@gmail.com>
2017-02-17 12:18:14 +0100

    yaml parser for nested dict corrected

--- TODO/NOTES/parse_yaml.py
+++ TODO/NOTES/parse_yaml.py
@@ -13,19 +13,14 @@ with open("config.yaml", 'r') as stream:
 def process_parsed_values(d): 
     indices = ['set','run','major','minor']     
     for i in indices:
-        try:
-            if d.get(i) is not None:         
-                if 'range' in str(d[i][0]):                       
-                    x = d[i][1]
-                    if len(x)<3: x.append(1) 
-                    d[i] = range(x[0],x[1],x[2])
-            else:
-                print("Required parameters missing in the config.yaml file!")
-                sys.exit(1)
-        except AttributeError:
-            print "error bhayo bhai"
-            # some inputs are not a dict, but just a string i.e. input, output path so skipping
-            pass
+        if d.get(i) is not None:         
+            if 'range' in str(d[i][0]):                       
+                x = d[i][1]
+                if len(x)<3: x.append(1) 
+                d[i] = range(x[0],x[1],x[2])
+        else:
+            print("Required parameters missing in the config.yaml file!")
+            sys.exit(1)
     return d
 
 for key in d.keys():
@@ -35,8 +30,8 @@ for key in d.keys():
             d_plt = inner_d[k]
             #print d_plt 
             param = process_parsed_values(d_plt)    
-    #print param['plot properties']['number_plots']
-    #print param['analysis']
+            print param['variables']['var2']
+            print param['analysis']
 
 #print NP.single
 
