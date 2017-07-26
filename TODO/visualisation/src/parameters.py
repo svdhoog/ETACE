76a2c2e78db48c94e1a6af972f36041f49d42e08
Krishna Devkota <krdevkota1@gmail.com>
2017-07-26 20:34:53 +0200

    moved plot parameters to parameters module

--- TODO/visualisation/src/parameters.py
+++ TODO/visualisation/src/parameters.py
@@ -19,13 +19,13 @@ class NP:
     # 0, 1
 
 
-class main_configuration:    
+class main_configuration():    
     
     def __init__(self):                 
-        self.main_f = 'config.yaml'   # the main configuration file
+        self.main_f = 'config1.yaml'   # the main configuration file
         self.parsed_values = self.parse_yaml(self.main_f)
     
-    def erf(msg): # function to output the error message and exit
+    def erf(self, msg): # function to output the error message and exit
         print " >> Error: %s" % msg
         sys.exit()
         
@@ -91,50 +91,100 @@ class main_configuration:
         if 'i/o' in self.parsed_values.keys():
             return self.parsed_values['i/o']['output_path']
         return self.erf("Missing output path!") 
-                
 
 
-def process_string( string_in ):
-    def find_between( s, first, last ):
+
+class Plot_configuration():
+
+    def __init__(self):
+        self.plot_f = 'plot_config.yaml'
+        self.__param = self.parse_yaml(self.plot_f)
+ 
+        
+    def erf(self, msg): 
+        print " >> Error: %s" % msg
+        sys.exit()
+
+    def parse_yaml(self,fname): 
         try:
-            start = s.index( first ) + len( first )
-            end = s.index( last, start )
-            return s[start:end]
-        except ValueError:
-            return ""    
-    operator = string_in.partition("[")[0]
-    string_out = find_between(string_in,"[","]")
-    return list([operator,float(string_out)])
+            f = open(fname, 'r')
+        except IOError:
+            erf("unable to read file: %s" % fname)
+        with f as stream:
+            try:
+                p = yaml.load(stream)
+            except yaml.YAMLError, exc: # error-check for incorrect yaml syntax
+                if hasattr(exc, 'problem_mark'):
+                    mark = exc.problem_mark
+                    print " >> Error in line: (%s:%s) in file: %s" % (mark.line+1, mark.column+1, fname)
+                else:
+                    print " >> Unknown problem with %s file:" % fname
+                sys.exit()
+            return p         
+
+    def legend(self, key):
+        return self.__param[key]['plot_legend']
+
+    def legend_label(self, key):
+        return self.__param[key]['legend_label']
+
+    def plot_type(self, key):
+        return self.__param[key]['plot_type']
+
+    def num_plots(self, key):
+        return self.__param[key]['number_plots']
+
+    def y_label(self, key):
+        return self.__param[key]['y-axis label']
+
+    def x_label(self, key):
+        return self.__param[key]['x-axis label']
+
+    def plot_name(self, key):
+        return self.__param[key]['plot_name']
+
+    def llim(self, key):
+        return self.__param[key]['l_lim']
+
+    def ulim(self, key):
+        return self.__param[key]['u_lim']
+
+    def linestyle(self, key):
+        return self.__param[key]['linestyle']
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
                     
 if __name__ == "__main__":
     C = main_configuration()
-    #print C.get_plot_type()
-    #print C.get_parameters()
-    for k, v in C.get_parameters().items():
-        print k
-        #print v['variables']
-        var_dic = {}
-        for i, j in v['variables'].items():
-            #print j[0]
-            if len(j)>1:
-                var_filter_list = []
-                for s in range(1,len(j)):
-                    var_filter_list.append(process_string(j[s]))
-                var_dic[j[0]] = var_filter_list
-            else:
-                var_dic[j[0]] = None
-
-        print C.get_pltname_for_idx(k) 
-    #print var_dic
-        #print v['minor']
-    #print C.map_idx_plotname()
-    #print C.input_fpath()
-    #print C.output_fpath()
-
+    
        
 
 
