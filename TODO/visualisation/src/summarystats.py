096ac73f92f75edc8aa9de600da78a2de4560c09
Krishna Devkota <krdevkota1@gmail.com>
2017-08-05 22:59:24 +0200

    changed custom quantile, terminal input removed

--- TODO/visualisation/src/summarystats.py
+++ TODO/visualisation/src/summarystats.py
@@ -23,7 +23,6 @@ class SummaryStats(A):
         self.__param = param
         self.__analysis_type = self.map_analysis()
 
-
     def compute_summary(self):
         summary_type = {'mean': self.mean, 'median': self.median, 'upper_quartile': self.upper_quartile,'lower_quartile': self.lower_quartile,'custom_quantile': self.custom_quantile,'minimum': self.minimum,'maximum': self.maximum}            
         return summary_type[self.__param['summary']]()
@@ -43,47 +42,37 @@ class SummaryStats(A):
         return options[self.__analysis_type]()
     
     def quantile(self, val):
-        agent_analysis = lambda : self.__data.groupby(level = ['set','run','major','minor']).quantile(val).dropna()
-        multiple_run_analysis = lambda : self.__data.groupby(level = ['set','run','major']).quantile(val).dropna()
-        multiple_batch_analysis = lambda : self.__data.groupby(level = ['set','major']).quantile(val).dropna()
-        multiple_set_analysis = lambda : self.__data.groupby(level=['major']).quantile(val).dropna()
+        agent_analysis = lambda : self.__data.groupby(level = ['set','run','major','minor']).quantile(val)#.dropna()
+        multiple_run_analysis = lambda : self.__data.groupby(level = ['set','run','major']).quantile(val)#.dropna()
+        multiple_batch_analysis = lambda : self.__data.groupby(level = ['set','major']).quantile(val)#.dropna()
+        multiple_set_analysis = lambda : self.__data.groupby(level=['major']).quantile(val)#.dropna()
         
         options = {A.agent : agent_analysis, A.multiple_run : multiple_run_analysis, A.multiple_batch : multiple_batch_analysis, A.multiple_set : multiple_set_analysis}          
         return options[self.__analysis_type]()  
     
     def custom_quantile(self):
-        try:
-            N = float(raw_input("Enter desired number of Quantiles. 1 for single quantile and 2 for both Upper and Lower quantile values : "))
-        except Exception:        
-            print ("Unrecognized input. Check input and try again!")
-            sys.exit(1)
-                
-        if N == 1:
+        #TODO: add exception handling for the input values
+        N = sorted(self.__param['quantile_values'].values())      
+        if len(N) == 1:
             s = pd.DataFrame()
-            try:
-                Q = float(raw_input("Input the desired quantile. Input format: 0.XX, where XX is the numeric quantile value you want: "))
-            except Exception:        
-                print ("Unrecognized input. Check input and try again!")
-                sys.exit(1)
-            s = s.append(self.quantile(Q)) # N is the custom value for Quantile that is needed
-            s.rename(columns = {list(s)[0]: "quantile ("+str(Q)+")"}, inplace = True)        
+            Q = float(N[0])
+            s = s.append(self.quantile(Q)) 
+            for i in range(len(list(s))):
+                s.rename(columns = {list(s)[i]: str(list(s)[i])+"_quantile ("+str(Q)+")"}, inplace = True)        
             return s
            
-        elif N == 2:
+        elif len(N) == 2:
             s1 = pd.DataFrame() # two df needed because df[xy] = z does not work as desired with multi-index
             s2 = pd.DataFrame()
-            try:
-                Q1 = float(raw_input("Input the desired Lower quantile. Input format: 0.XX, where XX is the numeric quantile value you want: "))
-                Q2 = float(raw_input("Input the desired Upper quantile. Input format: 0.XX, where XX is the numeric quantile value you want: "))
-            except Exception:        
-                print ("Unrecognized input. Check input and try again!")
-                sys.exit(1)
-            
+            Q1 = float(N[0])
+            Q2 = float(N[1])
             s1 = s1.append(self.quantile(Q1)) # data frame for lower quantile
-            s1.rename(columns = {list(s1)[0]: "lower_quantile ("+str(Q1)+")"}, inplace = True)
-
+            for i in range(len(list(s1))):
+                s1.rename(columns = {list(s1)[i]: str(list(s1)[i])+"_l_quantile ("+str(Q1)+")"}, inplace = True)
+            
             s2 = s2.append(self.quantile(Q2)) # data frame for upper quantile
-            s2.rename(columns = {list(s2)[0]: "upper_quantile ("+str(Q2)+")"}, inplace = True)
+            for i in range(len(list(s2))):
+                s2.rename(columns = {list(s2)[i]: str(list(s2)[i])+"_u_quantile ("+str(Q2)+")"}, inplace = True)
 
             return pd.concat([s1,s2], axis = 1)
 
