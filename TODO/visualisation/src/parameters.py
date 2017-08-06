ede9e68f16f874f285ff57a28fb5a79364a769b4
Krishna Devkota <krdevkota1@gmail.com>
2017-08-06 04:59:22 +0200

    trimmed code

--- TODO/visualisation/src/parameters.py
+++ TODO/visualisation/src/parameters.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
-import sys, os, yaml
+import sys
+import yaml
 
 
 class A:
@@ -8,10 +8,12 @@ class A:
     agent, multiple_run, multiple_batch, multiple_set = range(4)
     # 0, 1, 2, 3
 
+
 class M:
     # Types of statistical summary
     mean, median, upper_quartile, lower_quartile, custom_quantile, minimum, maximum = range(7)
     # 0, 1, 2, 3, 4, 5
+
 
 class NP:
     # Nature of Plots i.e. single output per analysis or multiple outputs per analysis
@@ -19,60 +21,53 @@ class NP:
     # 0, 1
 
 
+class main_configuration():
 
-
-
-
-
-
-class main_configuration():    
-    
-    def __init__(self):                 
-        self.main_f = 'config.yaml'   # the main configuration file
+    def __init__(self):
+        self.main_f = 'config.yaml'  # the main configuration file
         self.parsed_values = self.parse_yaml(self.main_f)
-    
-    def erf(self, msg): # function to output the error message and exit
+
+    def erf(self, msg):  # function to output the error message and exit
         print " >> Error: %s" % msg
         sys.exit()
-        
-    def parse_yaml(self,fname):  # Function to parse input parameters from the main configuration file
+
+    def parse_yaml(self, fname):  # Function to parse input parameters from the main configuration file
         try:
             f = open(fname, 'r')
         except IOError:
-            erf("unable to read file: %s" % fname)
+            self.erf("unable to read file: %s" % fname)
         with f as stream:
             try:
                 p = yaml.load(stream)
-            except yaml.YAMLError, exc: # error-check for incorrect yaml syntax
+            except yaml.YAMLError, exc:  # error-check for incorrect yaml syntax
                 if hasattr(exc, 'problem_mark'):
                     mark = exc.problem_mark
                     print " >> Error in line: (%s:%s) in file: %s" % (mark.line+1, mark.column+1, fname)
                 else:
                     print " >> Unknown problem with %s file:" % fname
                 sys.exit()
-            return p                
+            return p
 
-    
-    def process_parameters(self, p): # Function to process the parsed configuration file values to make them usable
-        indices = ['set','run','major','minor']    
+    def process_parameters(self, p):  # Function to process the parsed configuration file values to make them usable
+        indices = ['set', 'run', 'major', 'minor']
         for i in indices:
             if 'range' in str(p[i][0]):  # check if range defined in input configuration file
                 x = p[i][1]
-                try:              
-                    if len(x)<3: x.append(1) # if no step size defined in input, add default stepsize of one
-                    p[i] = range(x[0],x[1]+x[2],x[2]) # include last value, to make range of yaml file inclusive
+                try:
+                    if len(x) < 3: x.append(1)  # if no step size defined in input, add default stepsize of one
+                    p[i] = range(x[0], x[1] + x[2], x[2])  # include last value, to make range of yaml file inclusive
                 except:
-                    raise AssertionError("In file %s: range expects a list, single value given instead" % main_f)
+                    raise AssertionError("In file %s: range expects a list, single value given instead" % self.main_f)
         return p
 
-    def map_idx_plotname(self):        
+    def map_idx_plotname(self):
         d = {}
         for key in self.parsed_values.keys():
             if key not in 'i/o':
                 inner_dic = self.parsed_values[key]
-                for k in inner_dic.keys():               
-                    d[key] = k     
-        return d        
+                for k in inner_dic.keys():
+                    d[key] = k
+        return d
 
     def get_plotname_by_idx(self, idx):
         inner_dic = self.parsed_values[idx]
@@ -87,17 +82,16 @@ class main_configuration():
                 for v in inner_dic.values():
                     d[key] = self.process_parameters(v)
         return d
-            
+
     def input_fpath(self):
         if 'i/o' in self.parsed_values.keys():
             return self.parsed_values['i/o']['input_path']
-        return self.erf("Missing input path!") 
+        return self.erf("Missing input path!")
 
     def output_fpath(self):
         if 'i/o' in self.parsed_values.keys():
             return self.parsed_values['i/o']['output_path']
-        return self.erf("Missing output path!") 
-
+        return self.erf("Missing output path!")
 
 
 class Plot_configuration():
@@ -105,28 +99,27 @@ class Plot_configuration():
     def __init__(self):
         self.plot_f = 'plot_config.yaml'
         self.__param = self.parse_yaml(self.plot_f)
- 
-        
-    def erf(self, msg): 
+
+    def erf(self, msg):
         print " >> Error: %s" % msg
         sys.exit()
 
-    def parse_yaml(self,fname): 
+    def parse_yaml(self, fname):
         try:
             f = open(fname, 'r')
         except IOError:
-            erf("unable to read file: %s" % fname)
+            self.erf("unable to read file: %s" % fname)
         with f as stream:
             try:
                 p = yaml.load(stream)
-            except yaml.YAMLError, exc: # error-check for incorrect yaml syntax
+            except yaml.YAMLError, exc:  # error-check for incorrect yaml syntax
                 if hasattr(exc, 'problem_mark'):
                     mark = exc.problem_mark
                     print " >> Error in line: (%s:%s) in file: %s" % (mark.line+1, mark.column+1, fname)
                 else:
                     print " >> Unknown problem with %s file:" % fname
                 sys.exit()
-            return p         
+            return p
 
     def legend(self, key):
         return self.__param[key]['plot_legend']
@@ -162,7 +155,7 @@ class Plot_configuration():
         return self.__param[key]['linestyle']
 
     def marker(self, key):
-        return self.__param[key]['marker']   
+        return self.__param[key]['marker']
 
     def markersize(self, key):
         return self.__param[key]['markersize']
@@ -171,68 +164,5 @@ class Plot_configuration():
         return self.__param[key]['markerfacecolor']
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
-                    
 if __name__ == "__main__":
     C = main_configuration()
-    
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
