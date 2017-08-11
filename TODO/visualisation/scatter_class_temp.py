a80f5469656fddc921cca955f1f292804b6dcd69
Krishna Devkota <krdevkota1@gmail.com>
2017-08-12 00:28:50 +0200

    new file factoring scatterplot class, to be merged

---
+++ TODO/visualisation/scatter_class_temp.py
@@ -0,0 +1,126 @@
+from parameters import NP, A, Plot_configuration
+from summarystats import SummaryStats
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+class Plot(NP):  # TODO: remove NP here, inheritance usless for this class
+    def __init__(self, idx, data):
+        self.idx = idx
+        self.__data = data
+        self.__P = Plot_configuration()
+
+    def num_plot_mapper(self, val, obj):
+        one_plot = lambda: obj.one_output()
+        many_plot = lambda: obj.many_output()
+        options = {'one': one_plot, 'many': many_plot}
+        return options[val]()
+
+    def scatterplot(self, main_param, outpath):
+        S = Scatterplot(self.idx, self.__data, self.__P, main_param, outpath)
+        self.num_plot_mapper(self.__P.num_plots(self.idx), S)
+
+
+class Scatterplot(A):
+    
+    def __init__(self, idx, data, plt_config, main_param, outpath):
+        self.idx = idx  
+        self.__data = data
+        self.__P = plt_config
+        self.outpath = outpath          
+        self.__N = len(main_param['major'])
+        self.__analysistype = self.map_analysis(main_param['analysis'])
+        self.delay = main_param['delay']
+        self.summary = main_param['summary']
+        print "main data received from summary module to scatterplot module: "
+        print self.__data.tail(5)  
+        
+        if self.delay is True:
+            delayed_df = self.__data.shift(periods = 1, axis = 0)
+            delayed_df.rename(columns=lambda x: x+ "_delay", inplace=True)
+            D = pd.concat([self.__data, delayed_df], axis =1)            
+            self.__data = D[list(sum(zip(self.__data.columns, delayed_df.columns), ()))]
+
+
+    def map_analysis(self, val):             
+        analysis_values = {'agent' : A.agent, 'multiple_run' : A.multiple_run, 'multiple_batch' : A.multiple_batch, 'multiple_set' : A.multiple_set}    
+        return analysis_values[val]                  
+
+
+    def plot_line(self, ax, x, y, l_label, l_label2): 
+        if self.__P.legend_label(self.idx) is None:      
+            le_label = l_label
+        else:
+            le_label = self.__P.legend_label(self.idx)
+
+        out = ax.scatter(x, y, linestyle=self.__P.linestyle(self.idx), marker=self.__P.marker(self.idx), 
+        facecolor=self.__P.facecolors(self.idx), label=le_label)
+       
+        if self.__P.legend(self.idx) is True:
+            ax.legend(loc=self.__P.legend_location(self.idx), fancybox=True, shadow=True)
+        plt.xlabel(l_label)
+        plt.ylabel(l_label2)
+        return out
+
+    
+    def one_output(self):
+        file_count = 0
+        step = 2        
+        for col in range(0, len(self.__data.columns), step):
+            dframe = self.__data[[self.__data.columns[col], self.__data.columns[col+1]]].copy()
+           
+            if self.__analysistype == A.agent:
+                print "Agent wala analysis"
+                minor_index = dframe.index.get_level_values('minor').unique()                
+                legend_label = dframe.columns
+                fig, ax = plt.subplots() # initialize figure
+                for m in minor_index:
+                    D = dframe.xs(int(m), level='minor')                
+                    y1 = []
+                    y2 = []
+                    col_A = dframe[dframe.columns[0]]
+                    col_B = dframe[dframe.columns[1]]
+                    
+                    for i in range(0, len(dframe), self.__N):
+                        y1.append(np.array(col_A[i:i+self.__N]))
+
+                    for i in range(0, len(dframe), self.__N):
+                        y2.append(np.array(col_B[i:i+self.__N]))
+
+                    for r in range(0, len(dframe)/self.__N):
+                        self.plot_line(ax, y1[r], y2[r], legend_label[0]+'_inst_'+str(r), legend_label[1]+'_inst_'+str(r) )
+                        
+                    plt.legend(loc='best', fancybox=True, shadow=True)
+                    plot_name = self.__P.plot_name(self.idx)           
+                    plt.savefig(self.outpath + '/' + plot_name[:-4] + "_" + str(file_count) + ".png", bbox_inches='tight')
+                    plt.close()
+
+            else:
+                print "Aru khale analysis"
+                fig, ax = plt.subplots() # initialize figure
+                legend_label = dframe.columns
+                if len(dframe.columns) == 2:
+                    y1 = []
+                    y2 = []
+                    col_A = dframe[dframe.columns[0]]
+                    col_B = dframe[dframe.columns[1]]
+                    
+
+                    for i in range(0, len(dframe), self.__N):
+                        y1.append(np.array(col_A[i:i+self.__N]))
+
+                    for i in range(0, len(dframe), self.__N):
+                        y2.append(np.array(col_B[i:i+self.__N]))
+
+                    for r in range(0, len(dframe)/self.__N):
+                        self.plot_line(ax, y1[r], y2[r], legend_label[0]+'_inst_'+str(r), legend_label[1]+'_inst_'+str(r))
+                     
+
+                    plt.legend(loc='best', fancybox=True, shadow=True)
+                    plot_name = self.__P.plot_name(self.idx)           
+                    plt.savefig(self.outpath + '/' + plot_name[:-4] + "_" + str(file_count) + ".png", bbox_inches='tight')
+                    plt.close()
+                
+            file_count = file_count + 1
