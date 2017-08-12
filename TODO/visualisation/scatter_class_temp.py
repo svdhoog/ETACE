bcaecfc9d1e9155478a46b797045ce4311c4ca87
Krishna Devkota <krdevkota1@gmail.com>
2017-08-12 03:23:18 +0200

    single plot complete for all cases except agent analysis

--- TODO/visualisation/scatter_class_temp.py
+++ TODO/visualisation/scatter_class_temp.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
+import matplotlib.cm as cm
+
 
 class Plot(NP):  # TODO: remove NP here, inheritance usless for this class
     def __init__(self, idx, data):
@@ -35,8 +37,8 @@ class Scatterplot(A):
         self.delay = main_param['delay']
         self.summary = main_param['summary']
         print "main data received from summary module to scatterplot module: "
-        print self.__data.tail(5)  
-        
+        print self.__data.tail(5)        
+
         if self.delay is True:
             delayed_df = self.__data.shift(periods = 1, axis = 0)
             delayed_df.rename(columns=lambda x: x+ "_delay", inplace=True)
@@ -49,19 +51,20 @@ class Scatterplot(A):
         return analysis_values[val]                  
 
 
-    def plot_line(self, ax, x, y, l_label, l_label2): 
+    def plot_scatterplot(self, ax, x, y, l_label, x_label, y_label, clr):
+        
         if self.__P.legend_label(self.idx) is None:      
             le_label = l_label
         else:
             le_label = self.__P.legend_label(self.idx)
 
         out = ax.scatter(x, y, linestyle=self.__P.linestyle(self.idx), marker=self.__P.marker(self.idx), 
-        facecolor=self.__P.facecolors(self.idx), label=le_label)
+        facecolor=self.__P.facecolors(self.idx), label=le_label, color=clr)
        
         if self.__P.legend(self.idx) is True:
             ax.legend(loc=self.__P.legend_location(self.idx), fancybox=True, shadow=True)
-        plt.xlabel(l_label)
-        plt.ylabel(l_label2)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         return out
 
     
@@ -69,6 +72,10 @@ class Scatterplot(A):
         file_count = 0
         step = 2        
         for col in range(0, len(self.__data.columns), step):
+            if len(self.__data.columns) < 2:
+                print "Problem with data! Either set delay to True, or specify atleast two variables to plot!"                
+                sys.exit(1)
+
             dframe = self.__data[[self.__data.columns[col], self.__data.columns[col+1]]].copy()
            
             if self.__analysistype == A.agent:
@@ -90,8 +97,10 @@ class Scatterplot(A):
                         y2.append(np.array(col_B[i:i+self.__N]))
 
                     for r in range(0, len(dframe)/self.__N):
-                        self.plot_line(ax, y1[r], y2[r], legend_label[0]+'_inst_'+str(r), legend_label[1]+'_inst_'+str(r) )
-                        
+                        colors = itertools.cycle(["r", "b", "g"])
+                        clr = next(colors)
+                        self.plot_line(ax, y1[r], y2[r], legend_label[0], legend_label[0]+'_inst_'+str(r), legend_label[1]+'_inst_'+str(r), clr )
+                        # TODO: find a better label name for default case, erroneous label in case of quantile values
                     plt.legend(loc='best', fancybox=True, shadow=True)
                     plot_name = self.__P.plot_name(self.idx)           
                     plt.savefig(self.outpath + '/' + plot_name[:-4] + "_" + str(file_count) + ".png", bbox_inches='tight')
@@ -101,26 +110,26 @@ class Scatterplot(A):
                 print "Aru khale analysis"
                 fig, ax = plt.subplots() # initialize figure
                 legend_label = dframe.columns
-                if len(dframe.columns) == 2:
-                    y1 = []
-                    y2 = []
-                    col_A = dframe[dframe.columns[0]]
-                    col_B = dframe[dframe.columns[1]]
-                    
+                if len(dframe.columns) != 2:
+                    print "Something wrong with data, check and retry!"
+                    sys.exit (1)
 
-                    for i in range(0, len(dframe), self.__N):
-                        y1.append(np.array(col_A[i:i+self.__N]))
-
-                    for i in range(0, len(dframe), self.__N):
-                        y2.append(np.array(col_B[i:i+self.__N]))
-
-                    for r in range(0, len(dframe)/self.__N):
-                        self.plot_line(ax, y1[r], y2[r], legend_label[0]+'_inst_'+str(r), legend_label[1]+'_inst_'+str(r))
-                     
-
-                    plt.legend(loc='best', fancybox=True, shadow=True)
-                    plot_name = self.__P.plot_name(self.idx)           
-                    plt.savefig(self.outpath + '/' + plot_name[:-4] + "_" + str(file_count) + ".png", bbox_inches='tight')
-                    plt.close()
+                y1 = []
+                y2 = []
+                col_A = dframe[dframe.columns[0]]
+                col_B = dframe[dframe.columns[1]]
+                
+                for i in range(0, len(dframe), self.__N):
+                    y1.append(np.array(col_A[i:i+self.__N]))
+                    y2.append(np.array(col_B[i:i+self.__N]))
+                colors = iter(cm.rainbow(np.linspace(0, 1, len(y1))))
+                for r in range(0, len(dframe)/self.__N):
+                    clr = next(colors)
+                    self.plot_scatterplot(ax, y1[r], y2[r], legend_label[0]+'_inst_'+str(r), legend_label[0], legend_label[1], clr)
+                    # TODO: find a better label name for default case, erroneous label in case of quantile values
+                plt.legend(loc='best', fancybox=True, shadow=True)
+                plot_name = self.__P.plot_name(self.idx)           
+                plt.savefig(self.outpath + '/' + plot_name[:-4] + "_" + str(file_count) + ".png", bbox_inches='tight')
+                plt.close()
                 
             file_count = file_count + 1
