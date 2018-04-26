97b43f2f92bf38adb8067658896659797c08a8bf
Fabian Herrmann <fabih94@gmail.com>
2018-04-26 14:54:20 +0200

    gendb.py executeable

--- src/data_conversion_scripts/xml_db/gendb.py
+++ src/data_conversion_scripts/xml_db/gendb.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # $Id: gendb.py 2145 2009-09-23 08:51:01Z lsc $
-# 
-# Copyright (c) 2009 STFC Rutherford Appleton Laboratory 
-# Author: Lee-Shawn Chin 
+#
+# Copyright (c) 2009 STFC Rutherford Appleton Laboratory
+# Author: Lee-Shawn Chin
 # Date  : July 2009
 #
 # NOTE: This is a proof-of-concept implementation. It was not designed for
@@ -21,17 +21,19 @@ import sqlite3
 outfile = "iters.db"
 
 def error(mesg):
-    print >>sys.stderr, ">>>>> (Error): %s" % mesg
+    #print >>sys.stderr, ">>>>> (Error): {0}".format(mesg)
+    print(">>>>> (Error): {0}".format(mesg), file=sys.stderr)
     sys.exit(1)
 
 def warn(mesg):
-    print >>sys.stderr, ">> (Warning): %s" % mesg
+    #print >>sys.stderr, ">> (Warning): {0}".format(mesg)
+    print(">> (Warning): {0}".format(mesg), file=sys.stderr)
 
 def file_exist(fname):
     f = glob(fname)
     if fname in f: return True
     else: return False
-    
+
 def get_node_text(node):
     if len(node.childNodes) != 1 or node.childNodes[0].nodeType != node.TEXT_NODE:
         error("getText() routine used on non-text XML node")
@@ -44,118 +46,119 @@ def get_agent_sql(data):
     # data["vars"] = {}
     #     data["vars"]["varname"] = value
     global agents
-    
+
     try:
         name = data["name"]
         vars = data["vars"]
         iter = data["iter"]
     except:
         error("BUG!! contact script author")
-        
+
     # check agent exists
     if name not in agents.keys():
-        error("Unknown agent found: %s" % name)
-    
+        error("Unknown agent found: {0}".format(name))
+
     # retrieve values in the right order
     keys = []
     values = []
     for v in vars.keys():
         if v == "name": continue
         if v not in agents[name]:
-            error("Unknown agent memory : %s.%s" % (name, v))
+            error("Unknown agent memory : {0}.{1}".format(name, v))
         keys.append(v)
         values.append(vars[v])
-    
+
     sql = []
-    sql.append("insert into %s" % name)
-    sql.append("(_ITERATION_NO,%s)" % ",".join(keys))
-    sql.append("values (%d,'%s');" % (iter, "','".join(values)))    
+    sql.append("insert into {0}".format(name))
+    sql.append("(_ITERATION_NO,%s)".format(",").join(keys))
+    sql.append("values (%d,'%s');" % (iter, "','".join(values)))
     return " ".join(sql)
 
 def load_iteration_file(itno, basedir, db):
-    
-    filename = os.path.join(basedir, "%d.xml"%itno)
-    print "   + Reading file %s" % filename
-    
+
+    filename = os.path.join(basedir, "{0}.xml".format(itno))
+    print("   + Reading file {0}".format(filename))
+
     if not file_exist(filename):
-        error("Strange! Input file disappeared : %s" % filename)
+        error("Strange! Input file disappeared : {0}".format(filename))
     try:
         file = open(filename, "r")
     except:
-        error("Could not open file %s" % filename)
-    
-    
-    db.execute("INSERT INTO _iters_(itno) VALUES (%d);" % i)
-    
+        error("Could not open file {0}".format(filename))
+
+
+    db.execute("INSERT INTO _iters_(itno) VALUES ({0});".format(i))
+
     data = {}
     in_xagent_tag = False
     line = file.readline()
     re_tag = re.compile(r"<(\w+)>([^<]*)</\1>")
-    
+
     while line:
         line = line.strip()
-        
+
         if not line: # ignore empty lines
              # get next line and restart loop
             line = file.readline()
             continue
-        
+
         if in_xagent_tag:
             if line == "</xagent>": # end if this agent def
                 in_xagent_tag = False
                 db.execute( get_agent_sql(data) ) # store data in db
                 del(data)
-                
+
             else: # next agent mem
                 match = re_tag.match(line)
-                if not match: error("Unknown line: %s" % line)
-                
+                if not match: error("Unknown line: {0}".format(line))
+
                 tag = match.group(1)
                 val = match.group(2)
-                
+
                 if tag == "name":
                     data["name"] = val
                 else:
                     data["vars"][tag] = val
-                    
+
         else: # outside xagent tag
             if line == "<xagent>":
                 in_xagent_tag = True
                 data = {}
                 data["iter"] = i
                 data["vars"] = {}
-        
-        
+
+
         line = file.readline() # get next line
-        
+
     file.close()
 
-    
+
 # get input arguments
 if len(sys.argv) != 3:
-    print >>sys.stderr, "Usage: %s <model.xml> <its_directory>" % sys.argv[0]
+    #print >>sys.stderr, "Usage: {0} <model.xml> <its_directory>".format(sys.argv[0])
+    print("Usage: {0} <model.xml> <its_directory>".format(sys.argv[0], file=sys.stderr))
     sys.exit(0)
 model  = sys.argv[1]
 itsdir = sys.argv[2]
 
 # check that model file exists
 if not file_exist(model):
-    error("Model file (%s) does not exist" % model)
-    
+    error("Model file ({0}) does not exist".format(model))
+
 # ensure directory exists
 if not os.path.isdir(itsdir):
-    error("Input directory (%s) does not exist" % itsdir)
+    error("Input directory ({0}) does not exist".format(itsdir))
 
 # Retrieve list of data files
 datafiles = glob(os.path.join(itsdir, "[0-9]*.xml"))
 if len(datafiles) == 0:
-    error("No data files found in %s" % itsdir)
+    error("No data files found in {0}".format(itsdir))
 
 # get list of iters from datafile names
-print ""
-print " - Scanning \"%s\"" % itsdir
+print("")
+print(" - Scanning \"%s\"".format(itsdir))
 iters = []
-iter_pattern = "%s%s*([0-9]+)\.xml" % (itsdir, os.sep)
+iter_pattern = "{0}{1}*([0-9]+)\.xml".format(itsdir, os.sep)
 iter_re = re.compile(iter_pattern)
 for df in datafiles:
     match = iter_re.match(df)
@@ -164,16 +167,16 @@ for df in datafiles:
         continue
     iters.append(int(match.group(1)))
 iters.sort()
-print " - Found iters : %s" % str(iters)[1:-1]
+print(" - Found iters : {0}".format(str(iters)[1:-1]))
 
 # Initialise database
-print " - Initialising database file : \"%s\"" % outfile
+print(" - Initialising database file : \"{0}\"".format(outfile))
 if file_exist(outfile):
-    print "   ... File already exist. Deleting."
+    print("   ... File already exist. Deleting.")
     try:
         os.remove(outfile)
     except:
-        error("Could not delete file %s" % outfile)
+        error("Could not delete file {0}".format(outfile))
 db = sqlite3.connect(outfile, isolation_level=None) # manage our own transactions
 
 # some performance tuning
@@ -182,7 +185,7 @@ db.execute("PRAGMA synchronous = OFF;")
 db.execute("PRAGMA journal_mode = OFF;")
 
 # parse xmml file to determine required database structure
-print " - Analysing model"
+print(" - Analysing model")
 agents = {}
 models = [model]
 model_count = 0
@@ -191,27 +194,27 @@ while len(models) > 0:
     model_count = model_count + 1
     if model_count == 1:
 	    dirname = os.path.dirname(fname)
-    
+
     # load xml file
-    print "   + parsing %s" % fname
+    print("   + parsing {0}".format(fname))
     try:
         dom = minidom.parse(fname)
     except IOError:
-        error("Unable to read model file (%s)" % fname)
+        error("Unable to read model file ({0})".format(fname))
     except ExpatError:
-        error("Invalid XML file (%s)" % fname)
-        
+        error("Invalid XML file ({0})".format(fname))
+
     # detect nested models
     nodes = dom.getElementsByTagName("model")
     for node in nodes:
         status = get_node_text(node.getElementsByTagName("enabled")[0])
         if status != "true": continue # disabled. ignore
-        
+
         # add nested model file to list of files
         modelfile = get_node_text(node.getElementsByTagName("file")[0])
         models.append(os.path.join(dirname, modelfile))
     del(nodes)
-    
+
     # detect agents
     nodes = dom.getElementsByTagName("xagent")
     for node in nodes:
@@ -223,44 +226,46 @@ while len(models) > 0:
         for var in vars:
             varlist.append(get_node_text(var.getElementsByTagName("name")[0]))
         del(vars)
-        
+
         # remove array brackets
-        for i in xrange(len(varlist)):
+        for i in range(len(varlist)):
             v = varlist[i]
             if "[" in v: varlist[i] = v[:v.index("[")]
-            
+
         # add agent data to out list
         if aname in agents.keys(): # agent already exist. Merge
-            for v in varlist: 
+            for v in varlist:
                 if v not in agents[aname]: agents[aname].append(v)
         else: # new agent
             agents[aname] = varlist
-    del(nodes)    
-    
-    # finished parsing this file 
+    del(nodes)
+
+    # finished parsing this file
     del(dom)
 
 # Create db tables
-print " - Initialising database structure"
-print "   + creating table : _iters_"
+print(" - Initialising database structure")
+print("   + creating table : _iters_")
 db.execute("BEGIN;")
 db.execute("CREATE TABLE _iters_ (itno)") # metadata table
+
 for a in agents: # agent data tables
-    sql = "CREATE TABLE %s (_ITERATION_NO,%s)" % (a, ",".join(agents[a]))
-    print "   + creating table : %s" % a
+    #sql = "CREATE TABLE %s (_ITERATION_NO,%s)" % (a, ",".join(agents[a]))
+    sql = "CREATE TABLE {0} (_ITERATION_NO{1})".format(a, ",".join(agents.get(a)))
+    print("   + creating table : {0}".format(a))
     db.execute(sql)
 db.execute("END;")
-    
+
 # Finally, load data from *.xml files
-print " - Importing snapshot data into database"
-for i in iters: 
+print(" - Importing snapshot data into database")
+for i in iters:
     db.execute("BEGIN;")
     load_iteration_file(i, itsdir, db)
     db.execute("END;")
 
 # Done!
-print " - DONE. Output file : %s" % outfile
-print ""
+print(" - DONE. Output file : {0}".format(outfile))
+print("")
 
 # close db
 db.commit()
